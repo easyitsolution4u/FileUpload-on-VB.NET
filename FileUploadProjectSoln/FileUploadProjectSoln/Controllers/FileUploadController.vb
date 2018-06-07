aa3a6199fe65c62d45c7bbcc15c8406e965c7009
saroj Adhikari <sadhikaria@gmail.com>
2018-06-06 22:09:52 -0400

    additional file added

--- FileUploadProjectSoln/FileUploadProjectSoln/Controllers/FileUploadController.vb
+++ FileUploadProjectSoln/FileUploadProjectSoln/Controllers/FileUploadController.vb
@@ -1,5 +1,10 @@
 ﻿Imports System.IO
 Imports System.Web.Mvc
+Imports Microsoft.Office.Interop.Word
+
+
+
+
 
 Namespace Controllers
     Public Class FileUploadController
@@ -69,16 +74,58 @@ Namespace Controllers
                 Dim contentType As String = postedFile.ContentType
                 Dim fileName As String = postedFile.FileName
                 Dim contents As Stream = postedFile.InputStream
-                Using sr As StreamReader = New StreamReader(contents)
 
-                    fileData = sr.ReadToEnd()
+                Dim fileExtension As String = Path.GetExtension(postedFile.FileName)
 
-                End Using
+                If fileExtension = ".docx" Or fileExtension = ".doc" Then
+                    fileData = processPostedFileWord(postedFile)
+
+
+                ElseIf fileExtension = ".xlsx" Or fileExtension = ".xls" Then
+
+                    fileData = ExcelFileReader.getExcelFile(postedFile.FileName)
+
+
+                Else
+                    Using sr As StreamReader = New StreamReader(contents)
+                        Dim wordContent As String = processPostedFileWord(postedFile)
+                        fileData = sr.ReadToEnd()
+                    End Using
+                End If
+
 
 
 
             End If
             Return fileData
         End Function
+
+
+        Private Shared Function processPostedFileWord(postedFile As HttpPostedFileBase) As String
+            Dim wordApplication As Application = New Application()
+            Dim wordDocument As Document = New Document()
+            Dim FilePath As String = Path.GetFullPath(postedFile.FileName)
+            Dim NA As System.Reflection.Missing = System.Type.Missing
+
+            Try
+                wordDocument = wordApplication.Documents.Open(FilePath, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)
+            Catch ex As Exception
+                Throw ex
+            End Try
+
+            Dim sb As StringBuilder = New StringBuilder()
+
+            For lineNumber As Integer = 0 To wordDocument.Paragraphs.Count - 1
+                Dim fileLineText As String = wordDocument.Paragraphs(lineNumber + 1).Range.Text.Trim()
+
+                If fileLineText <> String.Empty Then
+                    sb.AppendLine(fileLineText)
+                End If
+            Next
+            wordDocument.Close()
+            wordApplication.Quit()
+            Return Convert.ToString(sb)
+        End Function
+
     End Class
 End Namespace
---
+++ FileUploadProjectSoln/FileUploadProjectSoln/ExcelFileReader.vb
@@ -0,0 +1,55 @@
+﻿
+Imports System
+Imports System.Collections.Generic
+Imports System.IO
+Imports System.Linq
+Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.Threading.Tasks
+Imports Excel = Microsoft.Office.Interop.Excel
+
+Public Class ExcelFileReader
+
+    Public Shared Function getExcelFile(ByVal filePath As String) As String
+        Dim excelApplication As Excel.Application = New Excel.Application()
+
+        Dim excelBook As Excel.Workbook = excelApplication.Workbooks.Open(filePath)
+
+        Dim firstExcelWorksheet As Excel._Worksheet = excelBook.Sheets(1)
+        Dim xlRange As Excel.Range = firstExcelWorksheet.UsedRange
+        Dim totalRowsInFile As Integer = xlRange.Rows.Count
+        Dim totalColumnsCounts As Integer = xlRange.Columns.Count
+        Dim recordsBuilder As New StringBuilder()
+        For rowIndex As Integer = 1 To totalRowsInFile
+
+            For columnIndex As Integer = 1 To totalColumnsCounts
+                If xlRange.Cells(rowIndex, columnIndex) IsNot Nothing AndAlso xlRange.Cells(rowIndex, columnIndex).Value2 IsNot Nothing Then
+                    Dim value As String = xlRange.Cells(rowIndex, columnIndex).Value2.ToString()
+                    recordsBuilder.Append(value)
+                End If
+            Next
+            recordsBuilder.AppendLine()
+        Next
+
+        cleanUp(excelApplication, excelBook, firstExcelWorksheet, xlRange)
+
+        Return recordsBuilder.ToString()
+
+    End Function
+
+    Private Shared Sub cleanUp(excelApplication As Excel.Application, excelBook As Excel.Workbook, firstExcelWorksheet As Excel._Worksheet, xlRange As Excel.Range)
+        GC.Collect()
+        GC.WaitForPendingFinalizers()
+        Marshal.ReleaseComObject(xlRange)
+        Marshal.ReleaseComObject(firstExcelWorksheet)
+        excelBook.Close()
+        Marshal.ReleaseComObject(excelBook)
+        excelApplication.Quit()
+        Marshal.ReleaseComObject(excelApplication)
+    End Sub
+End Class
+
+
+
+
+
