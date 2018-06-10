e95481d06168cbb54eed36f9dfd21b48610c6f99
Your Name <sadhikaria@gmail.com>
2018-06-10 12:38:16 -0500

    a

---
+++ FileUploadProjectSoln/FileUploadProjectSoln/Controllers/PracticeController.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class PracticeController
+        Inherits Controller
+
+        ' GET: Practice
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+        ' GET: Practice/Details/5
+        Function Details(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' GET: Practice/Create
+        Function Create() As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Practice/Create
+        <HttpPost()>
+        Function Create(ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add insert logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Practice/Edit/5
+        Function Edit(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Practice/Edit/5
+        <HttpPost()>
+        Function Edit(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add update logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Practice/Delete/5
+        Function Delete(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Practice/Delete/5
+        <HttpPost()>
+        Function Delete(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add delete logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+    End Class
+End Namespace
